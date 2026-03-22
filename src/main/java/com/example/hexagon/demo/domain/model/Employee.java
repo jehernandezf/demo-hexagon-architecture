0a43e749d007e389c876adfc7b56e742ec79d7f1
jehernandezf <javier.hf2795@gmail.com>
2026-03-22 17:36:53 -0600

    Se omite dependencia lombok

--- src/main/java/com/example/hexagon/demo/domain/model/Employee.java
+++ src/main/java/com/example/hexagon/demo/domain/model/Employee.java
@@ -1,14 +1,6 @@
 package com.example.hexagon.demo.domain.model;
 
 import java.time.LocalDateTime;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Employee {
     private Long employeeId;
     private String firstName;
@@ -21,4 +13,142 @@ public class Employee {
     private Double commissionPct;
     private Long managerId;
     private Long departmentId;
+
+    public Employee() {
+    }
+    
+    public Employee(Long employeeId, String firstName, String lastName, String email, String phoneNumber,
+            LocalDateTime hireDate, String jobId, Double salary, Double commissionPct, Long managerId,
+            Long departmentId) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.hireDate = hireDate;
+        this.jobId = jobId;
+        this.salary = salary;
+        this.commissionPct = commissionPct;
+        this.managerId = managerId;
+        this.departmentId = departmentId;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public LocalDateTime getHireDate() {
+        return hireDate;
+    }
+
+    public String getJobId() {
+        return jobId;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public Double getCommissionPct() {
+        return commissionPct;
+    }
+
+    public Long getManagerId() {
+        return managerId;
+    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setHireDate(LocalDateTime hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public void setJobId(String jobId) {
+        this.jobId = jobId;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public void setCommissionPct(Double commissionPct) {
+        this.commissionPct = commissionPct;
+    }
+
+    public void setManagerId(Long managerId) {
+        this.managerId = managerId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((employeeId == null) ? 0 : employeeId.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Employee other = (Employee) obj;
+        if (employeeId == null) {
+            if (other.employeeId != null)
+                return false;
+        } else if (!employeeId.equals(other.employeeId))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        return true;
+    }
 }
--- src/main/java/com/example/hexagon/demo/infrastructure/adapters/out/persistence/entity/EmployeeEntity.java
+++ src/main/java/com/example/hexagon/demo/infrastructure/adapters/out/persistence/entity/EmployeeEntity.java
@@ -8,48 +8,180 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "EMPLOYEES")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class EmployeeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "EMPLOYEE_ID", nullable = false)
     private Long employeeId;
-    
+
     @Column(name = "FIRST_NAME")
     private String firstName;
-    
+
     @Column(name = "LAST_NAME", nullable = false)
     private String lastName;
-    
+
     @Column(name = "EMAIL", nullable = false)
     private String email;
 
     @Column(name = "PHONE_NUMBER")
     private String phoneNumber;
-    
+
     @Column(name = "HIRE_DATE", nullable = false)
     private LocalDateTime hireDate;
-    
+
     @Column(name = "JOB_ID", nullable = false)
     private String jobId;
-    
+
     @Column(name = "SALARY")
     private Double salary;
-    
+
     @Column(name = "COMMISSION_PCT")
     private Double commissionPct;
-    
+
     @Column(name = "MANAGER_ID")
     private Long managerId;
-    
+
     @Column(name = "DEPARTMENT_ID")
     private Long departmentId;
+
+    public EmployeeEntity() {
+    }
+
+    public EmployeeEntity(Long employeeId, String firstName, String lastName, String email, String phoneNumber,
+            LocalDateTime hireDate, String jobId, Double salary, Double commissionPct, Long managerId,
+            Long departmentId) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.hireDate = hireDate;
+        this.jobId = jobId;
+        this.salary = salary;
+        this.commissionPct = commissionPct;
+        this.managerId = managerId;
+        this.departmentId = departmentId;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public LocalDateTime getHireDate() {
+        return hireDate;
+    }
+
+    public String getJobId() {
+        return jobId;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public Double getCommissionPct() {
+        return commissionPct;
+    }
+
+    public Long getManagerId() {
+        return managerId;
+    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setHireDate(LocalDateTime hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public void setJobId(String jobId) {
+        this.jobId = jobId;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public void setCommissionPct(Double commissionPct) {
+        this.commissionPct = commissionPct;
+    }
+
+    public void setManagerId(Long managerId) {
+        this.managerId = managerId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((employeeId == null) ? 0 : employeeId.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        EmployeeEntity other = (EmployeeEntity) obj;
+        if (employeeId == null) {
+            if (other.employeeId != null)
+                return false;
+        } else if (!employeeId.equals(other.employeeId))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        return true;
+    }
 }
